bc0e8c8bd0d9c758be999623804b280b35774ee0
Loc Le <lpl212757@gmail.com>
2025-05-18 14:52:24 +0700

    Don't use dashboard any more

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,7 +8,6 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
         bigfile = { enabled = true },
-        dashboard = { enabled = true },
         lazygit = { enabled = false },
         notifier = { enabled = true },
         quickfile = { enabled = true },
