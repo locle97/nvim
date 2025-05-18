84e56e1723a38b5a397a01b833136b4ff0867c64
Loc Le <lpl212757@gmail.com>
2025-05-18 12:41:28 +0700

    Update config of telescope

--- lua/nvchad/configs/telescope.lua
+++ lua/nvchad/configs/telescope.lua
@@ -1,24 +1,33 @@
 dofile(vim.g.base46_cache .. "telescope")
 
 return {
-  defaults = {
-    prompt_prefix = "   ",
-    selection_caret = " ",
-    entry_prefix = " ",
-    sorting_strategy = "ascending",
-    layout_config = {
-      horizontal = {
-        prompt_position = "top",
-        preview_width = 0.55,
-      },
-      width = 0.87,
-      height = 0.80,
+    defaults = {
+        prompt_prefix = "   ",
+        selection_caret = " ",
+        entry_prefix = " ",
+        sorting_strategy = "ascending",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+            },
+            width = 0.87,
+            height = 0.80,
+        },
+        mappings = {
+            n = { ["q"] = require("telescope.actions").close },
+        },
+        path_display = {
+            "filename_first"
+        },
     },
-    mappings = {
-      n = { ["q"] = require("telescope.actions").close },
+    pickers = {
+        lsp_references = {
+            path_display = {
+                "filename_first"
+            }
+        }
     },
-  },
-
-  extensions_list = { "themes", "terms" },
-  extensions = {},
+    extensions_list = { "themes", "terms", "grapple" },
+    extensions = {},
 }
