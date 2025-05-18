c8945762b40b2dc3869ecc8239ca7fba47497b30
Loc Le <lpl212757@gmail.com>
2025-05-18 14:25:41 +0700

    Enable dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-    theme = "catppuccin",
+    theme = "aquarium",
     transparency = true,
 
     -- hl_override = {
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,6 +8,7 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
         bigfile = { enabled = true },
+        dashboard = { enabled = true },
         lazygit = { enabled = false },
         notifier = { enabled = true },
         quickfile = { enabled = true },
