c6477e170a197645468ca51d3e40a6e0fed28d44
Loc Le <lpl212757@gmail.com>
2025-06-01 13:10:00 +0700

    Update git factor

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,7 +3,7 @@ return {
         "kdheepak/lazygit.nvim",
         lazy = false,
         config = function()
-            vim.g.lazygit_floating_window_scaling_factor = 0.9
+            vim.g.lazygit_floating_window_scaling_factor = 1
         end
     },
     {
