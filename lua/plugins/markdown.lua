49d9eaebd6c8170da6927622fb527422b037f968
Loc Le <lpl212757@gmail.com>
2025-06-01 19:54:14 +0700

    Update markdown preview to using firefox

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,18 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        config = function()
+            vim.g.mkdp_browser = "firefox"
+            vim.keymap.set("n",
+                "<leader>mp",
+                "<cmd>MarkdownPreviewToggle<cr>",
+                { desc = "Markdown Preview Toggle" })
+        end,
+        ft = { "markdown" },
+    },
 }
