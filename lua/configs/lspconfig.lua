b7a325b4aa73d132e8f72fbf39b5f0f0aa97f041
Loc Le <lpl212757@gmail.com>
2025-06-08 12:12:34 +0700

    Enable vuev3 lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "omnisharp", "jsonls" }
-vim.lsp.enable(servers)
+local servers = { "html", "cssls", "ts_ls", "omnisharp", "jsonls", "vue_ls" }
 
-local lspconfig = require('lspconfig')
+vim.lsp.config('vue_ls', {
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    init_options = {
+        vue = {
+            hybridMode = false,
+        },
+    },
+})
+
+vim.lsp.enable(servers)
