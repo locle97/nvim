4d61f703c32adc49eea54986d4d6ead4455b072e
Loc Le <lpl212757@gmail.com>
2025-06-09 22:06:48 +0700

    Try to using blink CMP

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -11,23 +11,19 @@ return {
         end,
     },
     {
-        "zbirenbaum/copilot-cmp",
-        event = "InsertEnter",
-        config = function()
-            require("copilot_cmp").setup()
-        end
-    },
-    {
         "CopilotC-Nvim/CopilotChat.nvim",
         dependencies = {
-            { "zbirenbaum/copilot.lua" },                 -- or zbirenbaum/copilot.lua
+            { "zbirenbaum/copilot.lua" },                   -- or zbirenbaum/copilot.lua
             { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
         },
-        build = "make tiktoken",                      -- Only on MacOS or Linux
+        build = "make tiktoken",                            -- Only on MacOS or Linux
         lazy = false,
         opts = {
             -- See Configuration section for options
         },
         -- See Commands section for default commands if you want to lazy load on them
-    }
+    },
+    {
+        "giuxtaposition/blink-cmp-copilot",
+    },
 }
