0c43b713775ca7ba48c0a27fe371de29d61deae6
Le Phat Loc <34123125+locle97@users.noreply.github.com>
2025-07-11 19:12:11 +0700

    Update autocmds.lua

--- lua/nvchad/autocmds.lua
+++ lua/nvchad/autocmds.lua
@@ -25,3 +25,14 @@ autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
     end
   end,
 })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
