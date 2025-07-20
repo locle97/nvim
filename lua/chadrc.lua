a71d5451d6e55482f3680c1d004cd5c530f4983e
Loc Le <lpl212757@gmail.com>
2025-07-20 16:47:32 +0700

    Remove grapple

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,7 +7,7 @@ local M = {}
 
 M.base46 = {
     theme = "aquarium",
-    transparency = true,
+    transparency = false,
 
     -- hl_override = {
     -- 	Comment = { italic = true },
@@ -28,21 +28,8 @@ M.ui = {
         -- default/round/block/arrow separators work only for default statusline theme
         -- round and block will work for minimal theme only
         separator_style = "default",
-        order = { "mode", "file", "grapple", "git", "%=", "lsp_msg", "%=", "diagnostics", "rest", "lsp", "cwd", "cursor" },
-        modules = {
-            grapple = function()
-                return " %#Label#" .. require("grapple").statusline()
-            end,
-            rest = function()
-                local current_filetype = vim.bo.filetype
-                local absolute_path = vim.b._rest_nvim_env_file
-                if current_filetype == "http" and absolute_path ~= nil then
-                    local filename = absolute_path:match("^.+/(.+)$")
-                    return filename
-                end
-                return ""
-            end,
-        },
+        order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+        modules = {},
     },
 }
 
