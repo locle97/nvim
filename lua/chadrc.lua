0632327298110745c764cf59abbcf210ea689c07
Loc Le <lpl212757@gmail.com>
2026-02-09 13:53:45 +0700

    Change theme of statusline to default

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -26,7 +26,7 @@ M.ui = {
          lazyload = false
     },
     statusline = {
-        theme = "minimal", -- default/vscode/vscode_colored/minimal
+        theme = "default", -- default/vscode/vscode_colored/minimal
         -- default/round/block/arrow separators work only for default statusline theme
         -- round and block will work for minimal theme only
         separator_style = "default",
